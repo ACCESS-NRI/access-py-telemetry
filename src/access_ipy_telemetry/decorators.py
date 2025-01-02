d337738c22a190b6feb07629955a589745b1a5b9
Charles Turner <charles.turner@anu.edu.au>
2025-01-03 07:53:33 +0800

    Updated 'ipy_register_func' decorator to make adding extra fields work same as 'register_func'

--- src/access_ipy_telemetry/decorators.py
+++ src/access_ipy_telemetry/decorators.py
@@ -9,7 +9,11 @@ from pathlib import Path
 from .api import SERVER_URL, ApiHandler
 
 
-def ipy_register_func(func: Callable[..., Any], service: str) -> Callable[..., Any]:
+def ipy_register_func(
+    func: Callable[..., Any],
+    service: str,
+    extra_fields: Iterable[dict[str, Any]] | None = None,
+) -> Callable[..., Any]:
     """
     Decorator to register a function in the specified service and track usage
     using IPython events. This hides a lot of complexity which is more visible in
@@ -19,14 +23,23 @@ def ipy_register_func(func: Callable[..., Any], service: str) -> Callable[..., A
     ----------
     func : Callable
         The function to register.
-    register : TelemetryRegister
-        The telemetry register to use.
+    service : str
+        The name of the telemetry register to use.
+    extra fields : Iterable[dict[str, Any]], optional
+        Extra fields to add to the telemetry record. These can also be added after
+        the fact using the `add_extra_field` method.
 
     Returns
     -------
     Callable
         The function with the telemetry decorator.
     """
+
+    api_handler = ApiHandler()
+
+    extra_fields = extra_fields or []
+    for field in extra_fields:
+        api_handler.add_extra_field(service, field)
 
     def wrapper(*args: Any, **kwargs: Any) -> Any:
         telemetry_register = TelemetryRegister(service)
@@ -48,9 +61,12 @@ def register_func(
     Parameters
     ----------
     func : Callable
-        The function to track.
+        The function to register.
     service : str
-        The service to track the function in.
+        The name of the telemetry register to use.
+    extra fields : Iterable[dict[str, Any]], optional
+        Extra fields to add to the telemetry record. These can also be added after
+        the fact using the `add_extra_field` method.
 
     Returns
     -------
